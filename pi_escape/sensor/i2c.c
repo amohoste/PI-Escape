05ad82454daf07f676d59fd65726dbe91348fc41
Jorg Wieme <jorg.wieme@ugent.be>
2017-10-16 15:31:30 +0200

    Start sensor

---
+++ pi_escape/sensor/i2c.c
@@ -0,0 +1,11 @@
+#include "../sensor/i2c.h"
+
+int i2c_init_adapter(int addr)
+{
+	return 0;
+}
+
+/*int i2c_write_byte_data(int file, uint8_t reg, uint8_t data)
+{
+	return 0;
+}*/
--- pi_escape/sensor/i2c.h
+++ pi_escape/sensor/i2c.h
@@ -1,6 +1,8 @@
 #ifndef I2C
 #define I2C
 
-//TODO
+int i2c_init_adapter(int);
+int i2c_write_byte_data(int, uint8_t , uint8_t );
+int i2c_read_byte_data(int, uint8_t);
 
 #endif /* I2C */
