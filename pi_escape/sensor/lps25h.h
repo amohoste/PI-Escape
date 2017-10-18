bb7410c596512c6fff31d55534fd1b5cd05e05ec
Jorg Wieme <jorg.wieme@ugent.be>
2017-10-18 23:07:50 +0200

    I2C en Druk sensor geimplementeerd, library lokaal niet inladen

--- pi_escape/sensor/lps25h.h
+++ pi_escape/sensor/lps25h.h
@@ -5,6 +5,8 @@
 	Initializeert een i2c adapter(0x5c)
 	Initializeert calibratievariable
 	@param frequentie aantal frequentie(hz) je door de lps25h sensor stuurt
+		keuze tussen  0, 1, 7, 12, 25 bij een andere waarde 
+		default 7
 	@return 0 succeed, -1 failed
 */
 int lps25h_init(int);
@@ -22,4 +24,11 @@ int lps25h_read_pressure();
 */
 int lps25h_read_temperature();
 
+
+/*
+	Een switch die de globale variable voor frequentie instelt
+	@param frequentie in hexadecimaal
+	@return 0;
+*/
+int init_initValue(frequentie);
 #endif /* LPS25H */
