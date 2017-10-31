0c4836d190ab413afe4d94969d4675f708c1c522
amohoste <Amory.Hoste@UGent.be>
2017-10-31 12:19:57 +0100

    fix asserts

--- pi_escape/level/levelloadertester.c
+++ pi_escape/level/levelloadertester.c
@@ -4,18 +4,18 @@
 
 int test_filesdimensions() {
 	// Tutorials
-	assert(test_filedimensions(1, 7, 3) == 0);
-	assert(test_filedimensions(2, 7, 7) == 0);
-	assert(test_filedimensions(3, 9, 8) == 0);
-	assert(test_filedimensions(4, 9, 8) == 0);
-	assert(test_filedimensions(5, 9, 8) == 0);
-	assert(test_filedimensions(6, 9, 8) == 0);
-	assert(test_filedimensions(7, 9, 8) == 0);
+	assert(test_filedimensions(1, 7, 3) == 1);
+	assert(test_filedimensions(2, 7, 7) == 1);
+	assert(test_filedimensions(3, 9, 8) == 1);
+	assert(test_filedimensions(4, 9, 8) == 1);
+	assert(test_filedimensions(5, 9, 8) == 1);
+	assert(test_filedimensions(6, 9, 8) == 1);
+	assert(test_filedimensions(7, 9, 8) == 1);
 
 	// Games
-    assert(test_filedimensions(8, 11, 8) == 0);
-    assert(test_filedimensions(9, 10, 13) == 0);
-    assert(test_filedimensions(10, 13, 14) == 0);
+    assert(test_filedimensions(8, 11, 8) == 1);
+    assert(test_filedimensions(9, 10, 13) == 1);
+    assert(test_filedimensions(10, 13, 14) == 1);
 
     return 1;
 }
