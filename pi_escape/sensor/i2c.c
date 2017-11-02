ddeb89753a7d221582b0d507d530fecd7da0b4d9
Jorg Wieme <jorg.wieme@ugent.be>
2017-11-02 11:45:01 +0100

    Error gevonden

--- pi_escape/sensor/i2c.c
+++ pi_escape/sensor/i2c.c
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 #include <fcntl.h>
+#include <linux/dev-i2c.h>
+#include <sys/ioctl.h>
 
 #include "../sensor/i2c.h"
 
@@ -11,9 +13,9 @@ int i2c_init_adapter(int addr)
 {
 	int file = open(FILENAME, O_RDWR);
 
-	/* if (ioctl(file, 0x700, addr) < 0) {
+	if (ioctl(file, 0x700, addr) < 0) {
 		return -1;
-	}*/
+	}
 	return file;
 }
 
