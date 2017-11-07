419b0cabe3c14dfbf5b8001cef1fc32005bece9b
Jorg Wieme <jorg.wieme@ugent.be>
2017-11-07 20:22:08 +0100

    Global variable bug

--- pi_escape/sensor/lps25h.c
+++ pi_escape/sensor/lps25h.c
@@ -16,25 +16,25 @@
 #define CLEAN_START 0x20
 
 
-int file;
+int filelps25h;
 
 int lps25h_init(int frequentie)
 {
 	uint8_t status;
 
 	// file maken en configureer de slave
-	if ((file = i2c_init_adapter(ADDR)) == -1) {
+	if ((filelps25h = i2c_init_adapter(ADDR)) == -1) {
 		return -1;
 	}
-	i2c_write_byte_data(file, CTRL_REG1, CLEAN_START);
+	i2c_write_byte_data(filelps25h, CTRL_REG1, CLEAN_START);
 
 	//TODO: change 0x84 to frequentie
-	i2c_write_byte_data(file, CTRL_REG1, 0x84);
+	i2c_write_byte_data(filelps25h, CTRL_REG1, 0x84);
 
-	i2c_write_byte_data(file, CTRL_REG4, 0x01);
+	i2c_write_byte_data(filelps25h, CTRL_REG4, 0x01);
 	do {
 		usleep(2500);
-		status = i2c_read_byte_data(file, CTRL_REG4);
+		status = i2c_read_byte_data(filelps25h, CTRL_REG4);
 	} while (status != 0);
 	
 	return 0;
@@ -43,9 +43,9 @@ int lps25h_init(int frequentie)
 double lps25h_read_pressure()
 {
 	// bepalen van variablen
-	uint8_t press_out_H = i2c_read_byte_data(file, PRESS_OUT_H);
-	uint8_t press_out_L = i2c_read_byte_data(file, PRESS_OUT_L);
-	uint8_t press_out_XL = i2c_read_byte_data(file, PRESS_OUT_XL);
+	uint8_t press_out_H = i2c_read_byte_data(filelps25h, PRESS_OUT_H);
+	uint8_t press_out_L = i2c_read_byte_data(filelps25h, PRESS_OUT_L);
+	uint8_t press_out_XL = i2c_read_byte_data(filelps25h, PRESS_OUT_XL);
 
 	// variablen naar goed formaat omzetten
 	int32_t press_out_HLLX = press_out_H << 16 | press_out_L << 8 | press_out_XL;
@@ -59,8 +59,8 @@ double lps25h_read_temperature()
 {
 	//ophalen van waarde
 	// TODO vraag of dit klopt
-	uint8_t temp_out_L = i2c_read_byte_data(file, TEMP_OUT_L);
-	uint8_t temp_out_H = i2c_read_byte_data(file, TEMP_OUT_H);
+	uint8_t temp_out_L = i2c_read_byte_data(filelps25h, TEMP_OUT_L);
+	uint8_t temp_out_H = i2c_read_byte_data(filelps25h, TEMP_OUT_H);
 
 	// combinatie maken van h en l 
 	int16_t temp_out_HL = temp_out_H << 8 | temp_out_L;
