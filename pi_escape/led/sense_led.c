a2196c1f228d613f6e2ef6e6f64e64e2288b6d75
Jorg Wieme <jorg.wieme@ugent.be>
2017-11-07 19:55:02 +0100

    Completed led wait for issue #52 to merge

--- pi_escape/led/sense_led.c
+++ pi_escape/led/sense_led.c
@@ -1,6 +1,5 @@
 #ifdef RPI
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -24,21 +23,19 @@ void display_ledgrid(SPGM_RGBTRIPLE* ledgrid, const char* framebuffer) {
 	int file;
 	uint16_t *mapping;
 	uint16_t *pointer;
-	printf("%d\n", FILESIZE);
 	struct fb_fix_screeninfo device_info;
 	if ((file = open(framebuffer, O_RDWR)) == -1) {
-		printf("Reading file failed.");
 		return;
 	}
+	/*
 	if (ioctl(file, FBIOGET_FSCREENINFO, device_info) == -1) {
 		close(file);
-		printf("Ioctl failed.");
 		return;
 	}
+	*/
 	mapping = mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, file, 0);
 	if (mapping == MAP_FAILED) {
 		close(file);
-		printf("%c", MAP_FAILED);
 		return;
 	}
 
@@ -47,7 +44,6 @@ void display_ledgrid(SPGM_RGBTRIPLE* ledgrid, const char* framebuffer) {
 	for (int i = 0; i < LENGTH_BUFFER; i++) {
 		SPGM_RGBTRIPLE rgb = ledgrid[i];
 		*(pointer + i) = rgb.rgbRed << 11 | rgb.rgbGreen << 5 | rgb.rgbBlue;
-		// usleep(2500);
 	}
 }
 #endif // RPI
