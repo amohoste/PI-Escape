3a291be20cc4846ea38905c13af9cb24cdfd1368
tolauwae <Tom.Lauwaerts@UGent.be>
2017-11-07 21:11:30 +0100

    fix error

--- pi_escape/es/system/real_sensors_system.c
+++ pi_escape/es/system/real_sensors_system.c
@@ -1,17 +1,15 @@
-#include "real_sensors_system.h"
-
 #ifdef RPI
+#include "real_sensors_system.h"
 #include "../../sensor/lps25h.h"
 #include "../../sensor/hts221.h"
 #include "../../sensor/i2c.h"
-#endif // RPI
 
 #include <stdlib.h>
 #include <stdio.h>
 
 #include <assert.h>
 
-#define FREQUENCY 2;
+#define FREQUENCY 2
 
 RealSensorsSystem* system_real_sensors_alloc() {
     RealSensorsSystem* res = calloc(1, sizeof(RealSensorsSystem));
@@ -20,10 +18,8 @@ RealSensorsSystem* system_real_sensors_alloc() {
 }
 
 void system_real_sensors_init(RealSensorsSystem* system) {
-#ifdef RPI
 		hts221_init(FREQUENCY);
 		lps25h_init(FREQUENCY);
-#endif // RPI
 }
 
 
@@ -31,9 +27,8 @@ void system_real_sensors_free(RealSensorsSystem* system) {
 }
 
 void system_real_sensors_update(RealSensorsSystem* system, Engine* engine) {
-#ifdef RPI
 	engine->context.humidity = hts221_read_humidity();
 	engine->context.temperature = hts221_read_temperature();
 	engine->context.pressure = lps25h_read_pressure();
-#endif // RPI
 }
+#endif // RPI
