fda09617401a2b6ce5106ad41c498d6e00fd7dfa
Jorg Wieme <jorg.wieme@ugent.be>
2017-11-10 03:05:02 +0100

    Added clear and basic version

--- pi_escape/led/sense_led.c
+++ pi_escape/led/sense_led.c
@@ -15,6 +15,7 @@
 
 #define LENGTH_BUFFER 64
 #define FILESIZE (LENGTH_BUFFER * sizeof(SPGM_RGBTRIPLE))
+#define FRAMEBUFFER "/dev/fb1"
 
 // name is RPI
 // filesize is 192
@@ -27,7 +28,7 @@ void display_ledgrid(SPGM_RGBTRIPLE* ledgrid, const char* framebuffer) {
 	if ((ledgridFile = open(framebuffer, O_RDWR)) == -1) {
 		return;
 	}
-	if (ioctl(ledgridFile, FBIOGET_VSCREENINFO, device_info) == -1) {
+	if (ioctl(ledgridFile, FBIOGET_VSCREENINFO, &device_info) == -1) {
 		close(ledgridFile);
 		return;
 	}
@@ -38,10 +39,27 @@ void display_ledgrid(SPGM_RGBTRIPLE* ledgrid, const char* framebuffer) {
 	}
 
 	pointer = mapping;
-	memset(mapping, 0, FILESIZE);
 	for (int i = 0; i < LENGTH_BUFFER; i++) {
 		SPGM_RGBTRIPLE rgb = ledgrid[i];
 		*(pointer + i) = rgb.rgbRed << 11 | rgb.rgbGreen << 5 | rgb.rgbBlue;
 	}
 }
+
+void display_ledgrid_fb1(SPGM_RGBTRIPLE* ledgrid) {
+	display_ledgrid(ledgrid, FRAMEBUFFER);
+}
+
+void clear_ledgrid() {
+	SPGM_RGBTRIPLE ledgrid[64];
+	for (int i = 0; i <= 64; i++)
+	{
+		SPGM_RGBTRIPLE current;
+		current.rgbRed = 0;
+		current.rgbGreen = 0;
+		current.rgbBlue = 0;
+		ledgrid[i] = current;
+	}
+	display_ledgrid_fb1(ledgrid);
+}
+
 #endif // RPI
