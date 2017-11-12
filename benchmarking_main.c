c921335f01a3c2823fad6505b4edfebcf60966d5
tolauwae <Tom.Lauwaerts@UGent.be>
2017-11-12 15:45:06 +0100

    take file as argument in main

--- benchmarking_main.c
+++ benchmarking_main.c
@@ -10,75 +10,85 @@
 int main(int argc, char **argv){
 	#undef BENCHMARK
     // Get specified file
-	if(argc > 0) return -1;
-	char * benchmark_file = argv[0];
-	printf("%s\n", benchmark_file);
+	if (argc < 2) { 
+		printf("No file specified. Press enter to quit.\n");
+		getchar();
+		return -1;
+	}
+	char * benchmark_file = argv[1];
+	printf("%i argument(s): %s\n", argc, benchmark_file);
+
+	FILE * file = fopen(benchmark_file, "r");
+	if (file == NULL) { 
+		printf("No such file. Press enter to quit.\n"); 
+		getchar();
+		return -1;
+	}
 
 	// Start simulation
-	//printf("Benchmark simulation started ...\n");
+	printf("Benchmark simulation started ...\n");
 
-	//clock_t start, end;
-	//double cpu_time_used;
-	//start = clock();
+	clock_t start, end;
+	double cpu_time_used;
+	start = clock();
 
-	//Engine* engine = calloc(1, sizeof(Engine));
-	//es_memory_manager_init(&engine->es_memory);
+	Engine* engine = calloc(1, sizeof(Engine));
+	es_memory_manager_init(&engine->es_memory);
 
-	//// Go over all records in the file
-	//FILE * file = fopen("./benchmarks/benchmarks.txt", "r");
-	//char line[MAX_RECORD_SIZE];
+	// Go over all records in the file
+	char line[MAX_RECORD_SIZE];
 
-	//int count = 0;
+	int count = 0;
 
-	//while (!feof(file)) {
-	//	count++;
-	//	int a = -1;
-	//	int b = -1;
-	//	int c = -1;
+	while (!feof(file)) {
+		count++;
+		int a = -1;
+		int b = -1;
+		int c = -1;
 
-	//	// Read line
-	//	fgets(line, MAX_RECORD_SIZE, file);
-	//	sscanf(line, "%i %i %i", &a, &b, &c);
-	//	Functions function = (Functions)a;
+		// Read line
+		fgets(line, MAX_RECORD_SIZE, file);
+		sscanf(line, "%i %i %i", &a, &b, &c);
+		Functions function = (Functions)a;
 
-	//	// perform logged function
-	//	switch (function)
-	//	{
-	//		case HAS_COMPONENT: {
-	//			has_component(engine, b, c);
-	//			break;
-	//		}
-	//		case GET_COMPONENT: {
-	//			get_component(engine, b, c);
-	//			break;
-	//		}
-	//		case CREATE_COMPONENT: {
-	//			create_component(engine, b, c);
-	//			break;
-	//		}
-	//		case FREE_COMPONENT: {
-	//			free_component(engine, b, c);
-	//			break;
-	//		}
-	//		case GET_NEW_ENTITY_ID: {
-	//			get_new_entity_id(engine);
-	//			break;
-	//		}
-	//		case NEW_LEVEL: {
-	//			es_memory_manager_init(&engine->es_memory);
-	//			break;
-	//		}
-	//		default: {
-	//			break;
-	//		}
-	//	}
-	//}
+		// perform logged function
+		switch (function)
+		{
+			case HAS_COMPONENT: {
+				has_component(engine, b, c);
+				break;
+			}
+			case GET_COMPONENT: {
+				get_component(engine, b, c);
+				break;
+			}
+			case CREATE_COMPONENT: {
+				create_component(engine, b, c);
+				break;
+			}
+			case FREE_COMPONENT: {
+				free_component(engine, b, c);
+				break;
+			}
+			case GET_NEW_ENTITY_ID: {
+				get_new_entity_id(engine);
+				break;
+			}
+			case NEW_LEVEL: {
+				es_memory_manager_init(&engine->es_memory);
+				break;
+			}
+			default: {
+				break;
+			}
+		}
+	}
 
-	//end = clock();
-	//cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+	end = clock();
+	cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-	//printf("\nsimulation finished. %i calls in %f seconds.\n", count, cpu_time_used);
-	printf("Press any key to continue...");
+	printf("\nsimulation finished. %i calls in %f seconds.\n", count, cpu_time_used);
+	printf("Press enter to continue...");
 	getchar();
 
     return 0;
