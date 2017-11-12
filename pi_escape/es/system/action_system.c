f79c9996a252cb83507ab126eefc491334835477
amohoste <Amory.Hoste@UGent.be>
2017-11-12 18:43:50 +0100

    add some whiles

--- pi_escape/es/system/action_system.c
+++ pi_escape/es/system/action_system.c
@@ -24,42 +24,44 @@ void system_action_update(ActionSystem* system, Engine* engine) {
 	// Locatie speler opvragen
 	EntityIterator player_it;
 	search_entity_2(engine, COMP_GRIDLOCATION, COMP_INPUTRECEIVER, &player_it);
-	next_entity(&player_it);
-	EntityId player_entity_id = player_it.entity_id;
-	assert(player_entity_id != NO_ENTITY);
-	InputReceiverComponent* player_input = get_component(engine, player_entity_id, COMP_INPUTRECEIVER);
 	
-	if (player_input->actkey && system->prevAct == 0) {
-		system->prevAct = 1;
-		GridLocationComponent* player_grid_comp = get_component(engine, player_entity_id, COMP_GRIDLOCATION);
-		int player_x = player_grid_comp->pos[0];
-		int player_y = player_grid_comp->pos[1];
+	while (next_entity(&player_it)) {
+		EntityId player_entity_id = player_it.entity_id;
+		assert(player_entity_id != NO_ENTITY);
+		InputReceiverComponent* player_input = get_component(engine, player_entity_id, COMP_INPUTRECEIVER);
 
-		EntityListIterator key_it;
-		start_search_in_list(player_x, player_y, engine, &key_it);
-		add_component_constraint(&key_it, 1, COMP_ITEMACTION);
+		if (player_input->actkey && system->prevAct == 0) {
+			system->prevAct = 1;
+			GridLocationComponent* player_grid_comp = get_component(engine, player_entity_id, COMP_GRIDLOCATION);
+			int player_x = player_grid_comp->pos[0];
+			int player_y = player_grid_comp->pos[1];
 
-		while (next_in_list_mask(&key_it)) {
-			EntityId itemaction_entity_id = key_it.entity_id;
-			assert(itemaction_entity_id != NO_ENTITY);
+			EntityListIterator key_it;
+			start_search_in_list(player_x, player_y, engine, &key_it);
+			add_component_constraint(&key_it, 1, COMP_ITEMACTION);
 
-			if (!has_component(engine, itemaction_entity_id, COMP_INCONTAINER)) {
-				create_component(engine, itemaction_entity_id, COMP_INCONTAINER);
-			}
-			else {
-				// Kijken of we niet op deur neerzetten
-				EntityListIterator door_it;
-				start_search_in_list(player_x, player_y, engine, &door_it);
-				add_component_constraint(&door_it, 1, COMP_BLOCKING);
-				if (!next_in_list_mask(&door_it)) {
-					free_component(engine, itemaction_entity_id, COMP_INCONTAINER);
+			while (next_in_list_mask(&key_it)) {
+				EntityId itemaction_entity_id = key_it.entity_id;
+				assert(itemaction_entity_id != NO_ENTITY);
+
+				if (!has_component(engine, itemaction_entity_id, COMP_INCONTAINER)) {
+					create_component(engine, itemaction_entity_id, COMP_INCONTAINER);
+				}
+				else {
+					// Kijken of we niet op deur neerzetten
+					EntityListIterator door_it;
+					start_search_in_list(player_x, player_y, engine, &door_it);
+					add_component_constraint(&door_it, 1, COMP_BLOCKING);
+					if (!next_in_list_mask(&door_it)) {
+						free_component(engine, itemaction_entity_id, COMP_INCONTAINER);
+					}
 				}
 			}
+
 		}
-		
-	}
-	else if (!player_input->actkey && system->prevAct == 1) {
-		system->prevAct = 0;
+		else if (!player_input->actkey && system->prevAct == 1) {
+			system->prevAct = 0;
+		}
 	}
 	
 }
