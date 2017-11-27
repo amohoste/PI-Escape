8b160efac0eec0d88995941407e6a13a19575cad
Arne Goeteyn <goeteynarne@gmail.com>
2017-11-27 18:28:41 +0100

    h files invullen grotendeels

--- pi_escape/anim/MenuBuilder.h
+++ pi_escape/anim/MenuBuilder.h
@@ -4,6 +4,23 @@
 #include "Animation.h"
 #include "Menu.h"
 
-//TODO
+class Menu;
+
+class EntryBuilder;
+
+class MenuBuilder : public EntryBuilder{
+public:
+    MenuBuilder& addEntry();
+    MenuBuilder& setEnabledOnPi(bool b);
+    MenuBuilder& setEnabledOnPc(bool b);
+    MenuBuilder& setLongText(const char* string);
+    MenuBuilder& setShortText(const char* string);
+    MenuBuilder& setFontName(const char* string);
+    MenuBuilder& setMnemonic(char c);
+    MenuBuilder& addAnimation(Animation animation);
+    MenuBuilder& buildEntryWithAction(const char* string);
+
+    MenuDefinition* build();
+};
 
 #endif //PIESCAPE2_MENUBUILDER_H
--- pi_escape/anim/MovieBuilder.h
+++ pi_escape/anim/MovieBuilder.h
@@ -1,9 +1,25 @@
 #ifndef PIESCAPE2_ANIMATIONSEQUENCEBUILDER_H
 #define PIESCAPE2_ANIMATIONSEQUENCEBUILDER_H
 
+#include <bits/shared_ptr.h>
 #include "Animation.h"
 #include "Movie.h"
 
-//TODO
+class EntryBuilder;
+
+class MovieBuilder : public EntryBuilder{
+public:
+    MovieBuilder& setDuration(long l);
+    MovieBuilder& addText(const char* string);
+    MovieBuilder& setFont(const char* string);
+    MovieBuilder& setColor(t_vec4 color);
+    MovieBuilder& setPos_percent(float x, float y);
+    MovieBuilder& setStartTime(long start);
+    MovieBuilder& setEndTime(long end);
+
+    std::shared_ptr<MovieDefinition> build();
+
+
+};
 
 #endif //PIESCAPE2_ANIMATIONSEQUENCEBUILDER_H
