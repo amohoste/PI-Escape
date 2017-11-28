253bd1f1b4d15e127b94fc367603b288a9a59e74
Arne Goeteyn <goeteynarne@gmail.com>
2017-11-28 11:23:49 +0100

    boilerplate code

--- pi_escape/anim/MenuBuilder.cpp
+++ pi_escape/anim/MenuBuilder.cpp
@@ -1,5 +1,15 @@
 #include "MenuBuilder.h"
+#include "EntryBuilder.h"
 
 using namespace std;
 
 
+EntryBuilder &MenuBuilder::addEntry() {
+    //todo
+    EntryBuilder skip;
+    return skip;
+}
+
+MenuDefinition *MenuBuilder::build() {
+    return nullptr;
+}
