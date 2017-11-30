d75d22377a4fd503eb50bdc6f1061b22897cf01b
Arne Goeteyn <goeteynarne@gmail.com>
2017-11-30 16:15:23 +0100

    boilerplatecode

--- pi_escape/anim/MenuBuilder.cpp
+++ pi_escape/anim/MenuBuilder.cpp
@@ -12,3 +12,44 @@ EntryBuilder &MenuBuilder::addEntry() {
 MenuDefinition *MenuBuilder::build() {
     return nullptr;
 }
+
+
+EntryBuilder &EntryBuilder::addAnimation(Animation *animation, MenuState activate, bool repeat, long duration) {
+    //todo
+    return *this;
+}
+
+EntryBuilder &EntryBuilder::setEnabledOnPc(bool b) {
+    //todo
+    return *this;
+}
+
+EntryBuilder &EntryBuilder::setEnabledOnPi(bool b) {
+    //todo
+    return *this;
+}
+
+EntryBuilder &EntryBuilder::setLongText(const char *text) {
+    //todo
+    return *this;
+}
+
+EntryBuilder &EntryBuilder::setShortText(const char *text) {
+    //todo
+    return *this;
+}
+
+EntryBuilder &EntryBuilder::setFontName(const char *text) {
+    //todo
+    return *this;
+}
+
+EntryBuilder &EntryBuilder::setMnemonic(char c) {
+    //todo
+    return *this;
+}
+
+EntryBuilder &EntryBuilder::buildEntryWithAction(const char *action) {
+    //todo
+    return *this;
+}
