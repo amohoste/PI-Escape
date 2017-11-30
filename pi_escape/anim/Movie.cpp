76e9810af1da9226c748254aa0843684497c85b8
Arne Goeteyn <goeteynarne@gmail.com>
2017-11-30 14:32:13 +0100

    std::move

--- pi_escape/anim/Movie.cpp
+++ pi_escape/anim/Movie.cpp
@@ -1,6 +1,9 @@
 #include "Movie.h"
 
+#include <utility>
+
 using namespace std;
 MovieDefinition::MovieDefinition(long duration, long start, long end, float x, float y, t_vec4& color, const char *font,
-                                 const char *text, vector<Animation*> animations) : duration(duration), start(start), end(end), x(x), y(y), color(color), font(font), text(text), animations(animations) {
+                                 const char *text, vector<Animation*> animations) : duration(duration), start(start), end(end), x(x), y(y), color(color), font(font), text(text), animations(
+        std::move(animations)) {
 }
