3942e7e0ec28cf92add1c7fd0baa6824c813da26
amohoste <Amory.Hoste@UGent.be>
2017-12-04 14:35:49 +0100

    fuck tvec

--- pi_escape/anim/Animation.h
+++ pi_escape/anim/Animation.h
@@ -49,7 +49,7 @@ public:
 /** Cycle through all colors */
 class RainbowColorAnimation : public Animation {
 private:
-	const t_vec3* hsv_to_rgb(float hue) const;
+	const float* hsv_to_rgb(float hue) const;
 public:
     RainbowColorAnimation();
     virtual ~RainbowColorAnimation();
