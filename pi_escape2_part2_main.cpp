d0a0b0f2623e8e8105f5da84840e70e02f296150
amohoste <Amory.Hoste@UGent.be>
2017-12-10 20:02:00 +0100

    remove debug

--- pi_escape2_part2_main.cpp
+++ pi_escape2_part2_main.cpp
@@ -21,13 +21,9 @@ extern "C"
 #include <SDL_timer.h>
 #include <string>
 
-#include <stdlib.h>
-#include <crtdbg.h>
-
 using namespace std;
 
 int main() {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
     int imgFlags = IMG_INIT_PNG;
     if (!(IMG_Init(imgFlags) & imgFlags)) {
