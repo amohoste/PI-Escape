3be1a358a6f610691ecb49f7bf483a2cc40af229
Jorg Wieme <jorg.wieme@ugent.be>
2017-12-10 12:39:59 +0100

    Implemented IMU for RPI

--- pi_escape/imu/RTIMU_c_wrapper.cpp
+++ pi_escape/imu/RTIMU_c_wrapper.cpp
@@ -1,3 +1,38 @@
-#include "RTIMU_c_wrapper.h"
+#ifdef RPI
+#include "../es/system/orientation_system.h"
+#include "RTIMULibDefs.h"
 
-//TODO
+
+RtImuSetting* create_ImuSetting() {
+	return reinterpret_cast<RtImuSetting*>(new RTIMUSettings("RTIMULib"));
+}
+
+void destroy_ImuSetting(RtImuSetting* struc) {
+	delete reinterpret_cast<RTIMUSettings*>(struc);
+}
+
+void destroy_RtImu(RtImu* struc) {
+	delete reinterpret_cast<RTIMU*>(struc);
+}
+
+RtImu* create_RtImu(RtImuSetting* settings) {
+	return reinterpret_cast<RtImu*>(RTIMU::createIMU(reinterpret_cast<RTIMUSettings*>(settings)));
+}
+
+void init_RtImu(RtImu* imu) {
+	reinterpret_cast<RTIMU*>(imu)->IMUInit();
+}
+
+int read_Imu(RtImu* imu) {
+	return reinterpret_cast<RTIMU*>(imu)->IMURead();
+}
+
+RTIMUDATA get_data(RtImu* imu) {
+	RTIMUDATA rtimu_data;
+	RTIMU_DATA data = reinterpret_cast<RTIMU*>(imu)->getIMUData();
+	rtimu_data.x = data.fusionPose.x();
+	rtimu_data.z = data.fusionPose.z();
+	return rtimu_data;
+}
+
+#endif // RPI
